dd4e5700fad284541b3ceee32119ef3b3205630e
Sanskar <sanskaramgain@gmail.com>
2022-02-14 19:36:03 +0545

    Project initialized

---
+++ boltzman.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def initialize_weights(n_visible: int, n_hidden: int) -> tuple(np.ndarray):
+    """
+        Initialize weight matrices of appropriate size 
+        n_visible: no of visible nodes
+        n_hidden: no of hidden nodes
+    """
+    W = None
+    b = None
+    c = None
+
+    raise NotImplementedError
+
+
