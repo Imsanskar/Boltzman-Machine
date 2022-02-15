0018be3c9e3df03fe2498697bc4ce37594f0499f
Sanskar <sanskaramgain@gmail.com>
2022-02-15 20:14:51 +0545

    Code refactored, and made more readable

--- boltzman.py
+++ boltzman.py
@@ -30,7 +30,7 @@ def initialize_weights(n_visible: int, n_hidden: int):
 
     return W, b, c
 
-def forward(W:np.ndarray, v_bias: np.ndarray, h_bias:np.ndarray, X_inp: np.ndarray):
+def train(W:np.ndarray, v_bias: np.ndarray, h_bias:np.ndarray, X_inp: np.ndarray, alpha:float):
     # assert so that the type is matched and no unexpected result are seen
     # could be used for only debugging approach and be remove later on
     if DEBUGGING:
@@ -42,16 +42,35 @@ def forward(W:np.ndarray, v_bias: np.ndarray, h_bias:np.ndarray, X_inp: np.ndarr
     assert W.shape[1] == h_bias.shape[0], "Size mismatch between W and hidden bias"
     assert W.shape[0] == v_bias.shape[0], "Size mismatch between W and visible bias"
 
-    _h0 = torch.sigmoid(torch.tensor(np.matmul(X_inp, W) + h_bias))
-    h0 = nn.ReLU(_h0 - torch.rand(_h0.shape))
+    # numpy to torch tensor
+    W = torch.tensor(W.copy(), dtype=float)
+    v_bias = torch.tensor(v_bias.copy(), dtype=float)
+    h_bias= torch.tensor(h_bias.copy(), dtype=float)
+    X_inp = torch.tensor(X_inp.copy(), dtype=float)
 
 
-    _v0 = nn.Sigmoid(torch.tensor(np.matmul(h0, np.transpose(W)) + v_bias))
-    v1 = nn.ReLU(_h0 - np.random.rand(_v0.shape))
-    h1 = nn.Sigmoid(np.matmul(v1, W) + h_bias)
+    _h0 = torch.sigmoid(torch.matmul(X_inp, W) + h_bias)
+    h0:torch.Tensor = torch.relu(_h0 - torch.rand(_h0.shape))
 
-    return h1
+
+    _v0 = torch.sigmoid(torch.tensor(torch.matmul(h0, torch.transpose(W, 0, 1)) +  + v_bias))
+    v1 = torch.relu(_v0 - torch.rand(_v0.shape))
+    h1 = torch.sigmoid(torch.matmul(v1, W) + h_bias)
+
+    w_pos_grad = torch.matmul(torch.transpose(X_inp.view(X_inp.shape[0], 1), 0, 1).view(X_inp.shape), h0)
+    w_neg_grad = torch.matmul(torch.transpose(v1, 0, 1), h1)
+
+    CD = (w_pos_grad - w_neg_grad) / torch.to_float(v1.shape[0])
     
+    update_w:torch.Tensor = W + alpha * CD
+    update_vb = v_bias + alpha * torch.reduce_mean(X_inp - v1, 0)
+    update_hb = h_bias + alpha * torch.reduce_mean(h0 - h1, 0)
+
+    err = X_inp - v1
+
+    err_sum = torch.reduce_mean(err * err)
+
+    return update_w.detach().cpu().numpy(), v_bias.detach().cpu().numpy(), h_bias.detach().cpu().numpy()
 
 if __name__ == "__main__":
     n_visible = 4
@@ -60,12 +79,13 @@ if __name__ == "__main__":
     #learning rate 
     alpha = 0.1
 
-
+    # intialize weight
     W, v_bias, h_bias = initialize_weights(n_visible, n_hidden)
 
-    forward(W, v_bias, h_bias, np.array([0, 1, 0, 1]))
 
+    W, v_bias, h_bias = train(W, v_bias, h_bias, np.array([0, 1, 0, 1]), alpha)
 
+    pass
     
 
 
--- boltzman_pytorch.py
+++ boltzman_pytorch.py
@@ -13,6 +13,11 @@ import torch.nn.functional as F
 import numpy as np
 from torchvision.utils import make_grid , save_image
 
+# various constants
+batch_size = 256
+
+
+
 class RBM(nn.Module):
 	def __init__(self, n_visible = 784, n_hidden = 500, k = 5) -> None:
 		super(RBM, self).__init__()
@@ -29,30 +34,45 @@ class RBM(nn.Module):
 
 
 	def v_to_h(self,v):
-		p_h = F.sigmoid(F.linear(v,self.W,self.h_bias))
+		"""
+			Calculates the probabilities of the hidden units
+		"""
+		p_h = torch.sigmoid(torch.matmul(v,self.W.t()) + self.h_bias)
 		sample_h = self.sample_from_vec(p_h)
-		return p_h,sample_h
+		return sample_h
 	
 
 	def h_to_v(self,h):
-		p_v = F.sigmoid(F.linear(h,self.W.t(),self.v_bias))
+		"""
+			Reconstruct input from hidden layer
+		"""
+		p_v = torch.sigmoid(torch.matmul(h, self.W) + self.v_bias)
 		sample_v = self.sample_from_vec(p_v)
-		return p_v,sample_v
+		return sample_v
 		
    	
 	def forward(self,v):
-		pre_h1,h1 = self.v_to_h(v)
+		"""
+			Forward propagation of the model
+		"""
+		h1 = self.v_to_h(v)
 		
-		h_ = h1
+		hidden_probability = h1
+
+		# k means how many times do we have to sample
 		for _ in range(self.k):
-			pre_v_,v_ = self.h_to_v(h_)
-			pre_h_,h_ = self.v_to_h(v_)
+			reconstructed_input = self.h_to_v(hidden_probability)
+			hidden_probability = self.v_to_h(reconstructed_input)
 		
-		return v,v_
+		return v,reconstructed_input
 	
-	def free_energy(self,v):
-		vbias_term = v.mv(self.v_bias)
-		wx_b = F.linear(v,self.W,self.h_bias)
+	def free_energy(self, inp):
+		"""
+			Calculates energy of the state
+		"""
+		# matrix vector multiplication of inp and v_bias
+		vbias_term = inp.mv(self.v_bias)
+		wx_b = torch.matmul(inp,self.W.t()) + self.h_bias
 		hidden_term = wx_b.exp().add(1).log().sum(1)
 		return (-hidden_term - vbias_term).mean()
 
@@ -71,6 +91,8 @@ if __name__ == "__main__":
 		transforms.Resize((28, 28)),
 		transforms.Grayscale()
 	])
+
+	# load all the dataset
 	handwritten_data = torchvision.datasets.ImageFolder("./data/handwrittendataset/Train/", transform)
 	dataloader = torch.utils.data.DataLoader(dataset=handwritten_data, batch_size = 128, shuffle = True)
 
@@ -81,9 +103,8 @@ if __name__ == "__main__":
 	for epoch in range(10):
 		loss_ = []
 		for i, (data,target) in enumerate(dataloader):
-			data = Variable(data.view(128, 3, 784))
+			data = Variable(data.view(data.size()[0], 784))
 			sample_data = data.bernoulli()
-			
 			v,v1 = rbm(sample_data)
 			loss = rbm.free_energy(v) - rbm.free_energy(v1)
 			loss_.append(loss.data)
